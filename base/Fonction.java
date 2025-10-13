35f3401c7f87e70cdf62f3802b7b01e5f8fceaba
Jérémy <jeremy.perrouault@ascadis.fr>
2025-10-13 10:28:16 +0000

    JOUR 1 - Fonctions

---
+++ base/Fonction.java
@@ -0,0 +1,20 @@
+
+// Les fonctions : un bloc d'instruction qu'on peut rappeler autant de fois qu'on veut
+// > Et pour lequel on peut donner des paramètres d'entrée
+// > La fonction peut (ou pas) retourner UNE valeur d'UN type
+
+// TypeDeRetour NomFonction(TypeParam1 param1, TypeParam2 param2)
+
+// Une fonction qui retourne "rien"
+void fonctionQuiRetourneRien() {
+
+}
+
+// Une fonction qui retourne un entier et qui en attend 2
+int fonctionQuiRetourneUnEntier(int a, int b) {
+
+}
+
+// Pour appeler la fonction :
+fonctionQuiRetourneRien();
+fonctionQuiRetourneUnEntier(5, 8);

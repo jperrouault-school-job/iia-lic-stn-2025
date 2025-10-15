4c9bb22ea1f280873b2be5162f5189ab98e8e79c
Jérémy <jeremy.perrouault@ascadis.fr>
2025-10-15 11:53:34 +0000

    JOUR 2 - CORRECTION EXO 5

---
+++ fonctionnel/AppExo5.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppExo5 {
+    public static void main(String[] args) {
+        List<Integer> nombres = new ArrayList<>();
+
+        nombres.add(4);
+        nombres.add(3);
+        nombres.add(5);
+        nombres.add(10);
+
+        nombres.stream()
+            .map(val -> val * val)
+            .filter(val -> val > 25 )
+            .forEach(System.out::println)
+        ;
+    }
+}

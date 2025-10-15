cfb5a253edf1ba9dbf3b9d8882cc476f803b9ddb
Jérémy <jeremy.perrouault@ascadis.fr>
2025-10-15 14:13:25 +0000

    JOUR 2 - CORRECTION EXO 8

---
+++ fonctionnel/AppExo8.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppExo8 {
+    public static void main(String[] args) {
+        List<Integer> nombres = new ArrayList<>();
+
+        nombres.add(4);
+        nombres.add(13);
+        nombres.add(15);
+        nombres.add(90);
+
+        // Solution #1
+        int resultat = nombres.stream()
+            .reduce(Integer.MAX_VALUE, (min, val) -> {
+                if (min < val) {
+                    return min;
+                }
+
+                return val;
+            })
+        ;
+
+        // Solution #2
+        int resultat2 = nombres.stream()
+            .reduce(Integer.MAX_VALUE, Integer::min)
+        ;
+
+        // Solution #3
+        int resultat3 = nombres.stream()
+            .reduce(Integer::min)
+            .get()
+        ;
+
+        // Solution #4
+        int resultat4 = nombres.stream()
+            .sorted()
+            .findFirst()
+            .get()
+        ;
+
+        System.out.println(resultat);
+        System.out.println(resultat2);
+    }
+}

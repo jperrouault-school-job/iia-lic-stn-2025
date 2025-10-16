c568dee72855abfebfc3dbdbfa915519f232d8d8
Jérémy <jeremy.perrouault@ascadis.fr>
2025-10-16 16:01:05 +0000

    JOUR 4 - CORRECTION EXO SUP. 4

--- corrections-exos-supplementaires/exo4/App.java
+++ corrections-exos-supplementaires/exo4/App.java
@@ -18,33 +18,43 @@ public class App {
 
         hp1.setAuteur("J.K. Rowling");
         hp1.setTitre("Harry Potter à l'école des Sorciers");
+        hp1.setNbPages(311);
 
         hp2.setAuteur("J.K. Rowling");
         hp2.setTitre("Harry Potter et la Chambre des Secrets");
+        hp2.setNbPages(460);
 
         hp3.setAuteur("J.K. Rowling");
         hp3.setTitre("Harry Potter et le Prisonnier d'Azkaban");
+        hp3.setNbPages(448);
 
         hp4.setAuteur("J.K. Rowling");
         hp4.setTitre("Harry Potter et la Coupe de Feu");
+        hp4.setNbPages(768);
 
         hp5.setAuteur("J.K. Rowling");
         hp5.setTitre("Harry Potter et l'Ordre des Phoenix");
+        hp5.setNbPages(1031);
 
         hp6.setAuteur("J.K. Rowling");
         hp6.setTitre("Harry Potter et le Prince de sang-mêlé");
+        hp6.setNbPages(720);
 
         hp7.setAuteur("J.K. Rowling");
         hp7.setTitre("Harry Potter et les Reliques de la Mort");
+        hp7.setNbPages(850);
 
         la1.setAuteur("Pierre Bottero");
         la1.setTitre("L'autre : Le souffle de la hyène");
+        la1.setNbPages(307);
 
         la2.setAuteur("Pierre Bottero");
         la2.setTitre("L'autre : Le Maître des tempêtes");
+        la2.setNbPages(360);
 
         la3.setAuteur("Pierre Bottero");
         la3.setTitre("L'autre : La huitième porte");
+        la3.setNbPages(384);
 
         biblio.ajouter(hp1);
         biblio.ajouter(hp2);

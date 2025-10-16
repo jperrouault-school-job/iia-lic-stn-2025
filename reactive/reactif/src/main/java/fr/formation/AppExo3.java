c88f0cee26d644c8722bfe03a9352d6548cc9930
Jérémy <jeremy.perrouault@ascadis.fr>
2025-10-16 09:47:50 +0000

    JOUR 4 - CORRECTION EXO 3

---
+++ reactive/reactif/src/main/java/fr/formation/AppExo3.java
@@ -0,0 +1,25 @@
+package fr.formation;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+public class AppExo3 {
+    public static void main(String[] args) throws Exception {
+        Sinks.Many<String> sinks = Sinks.many().multicast().directAllOrNothing();
+        Flux<String> flux = sinks.asFlux();
+
+        flux
+            // .map(saisie -> saisie.length())
+            .map(String::length)
+            .filter(nb -> nb > 5)
+            .subscribe(nbCars -> {
+                System.out.println("Il y a " + nbCars + " caractères.");
+            }
+        );
+
+        for (int i = 0; i < 5; i = i + 1) {
+            String laSaisie = System.console().readLine();
+            sinks.tryEmitNext(laSaisie);
+        }
+    }
+}
